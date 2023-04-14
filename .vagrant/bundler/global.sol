7a650cd8dfbf6f4285d2080de4a3815b2720a914
ChaosHour <kurt_lv@cox.net>
2023-04-13 23:27:52 -0700

    adding files

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["net-ssh",[">= 2.6.5","< 8.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"e3473e084aa9544851202089154fa1b29a2a17a2c968cbf315671aae92188e58","vagrant_version":"2.3.4"}
